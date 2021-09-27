4ec8a164731d85185f7f86988c47e89a4ef1506c
Clementine Rin <clementineRin09@gmail.com>
2021-09-27 14:00:54 +0700

    Change the default database to mysql.

--- dicdactic/settings.py
+++ dicdactic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import keyring
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^0t6ispcq0_%6k490o&4hx_=8q9n=o^mlfylxf^n6i$%yjur5m'
+SECRET_KEY = keyring.get_password('secret_key', 'robbi')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'dicdactic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dicdactic',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': keyring.get_password('database', 'root')
     }
 }
 
