73f35ef8e443a0b82be53f982c73dec00e1f1db6
Tokiyo <codewithwafik@gmail.com>
2021-09-28 05:55:11 +0700

    Define models in the movie app.
    Models:
        - Genre
        - Movie

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Movie(models.Model):
+    RATE_G = 'G'
+    RATE_PG = 'PG'
+    RATE_PG13 = 'PG13'
+    RATE_NC17 = 'NC17'
+    RATE_CHOICES = [
+        (RATE_G, 'G'),
+        (RATE_PG, 'PG'),
+        (RATE_PG13, 'PG-13'),
+        (RATE_NC17, 'NC-17'),
+    ]
+    title = models.CharField(max_length=255)
+    synopsis = models.TextField()
+    daily_rate = models.FloatField()
+    director = models.CharField(max_length=255)
+    rate = models.CharField(max_length=4, choices=RATE_CHOICES, default=RATE_G)
+    genres = models.ManyToManyField(Genre)
